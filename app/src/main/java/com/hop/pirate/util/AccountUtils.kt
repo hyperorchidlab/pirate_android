ccf745e73bc5a33180531687d94cd57edbbc04c7
alen-x <alen-x@tutanota.com>
2021-01-31 11:08:47 +0800

    Use kotlin method to read files

--- app/src/main/java/com/hop/pirate/util/AccountUtils.kt
+++ app/src/main/java/com/hop/pirate/util/AccountUtils.kt
@@ -2,8 +2,6 @@ package com.hop.pirate.util
 
 import android.content.Context
 import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
 
 object AccountUtils {
 
@@ -12,28 +10,7 @@ object AccountUtils {
         if (!file.exists()) {
             return ""
         }
-        var fileInputStream: FileInputStream? = null
-        val bytes = ByteArray(1024)
-        return try{
-            fileInputStream = FileInputStream(file)
-            val stringBuffer = StringBuffer()
-            var read = -1
-            while (fileInputStream.read(bytes).also { read = it } != -1) {
-                stringBuffer.append(String(bytes, 0, read))
-            }
-            fileInputStream.read()
-            stringBuffer.toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            ""
-        } finally {
-            try {
-                if (fileInputStream != null) {
-                    fileInputStream.close()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
+
+        return file.readText()
     }
 }
